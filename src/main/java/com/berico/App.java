04104236b50fcfecbe897445b89c3ea5c3a9cbfd
Richard Clayton <rclayton@bericotechnologies.com>
2012-09-23 19:39:51 -0400

    Maven project structure

---
+++ src/main/java/com/berico/App.java
@@ -0,0 +1,13 @@
+package com.berico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
